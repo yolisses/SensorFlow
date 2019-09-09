18b19fc7bf6d1fe4ee1cdc0ccd1d2e7b17cfb60a
William <williamjbf@gmail.com>
2019-09-09 08:58:23 -0300

    0.2

--- app/sensor/views.py
+++ app/sensor/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import (DetailView)
+
+from . import models
+
+class SensorCreateView(CreateView):
+    model= models.Sensor
+    template_name='HTML/sensores.html'
+    fields= ['name']
+    sucess_url = reverse_lazy('sensor-list')
+
+class SensorListView(ListView):
+
+    model= models.Sensor
+    template_name= 'HTML/sensores.html'
